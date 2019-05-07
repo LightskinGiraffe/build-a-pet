5d7981ce12802eaef1dec1e5d1ead6294cdbfa03
Sincere J Bolling <sincerebolling@gmail.com>
2019-05-06 19:23:46 -1000

    completed

---
+++ app.js
@@ -0,0 +1,58 @@
+window.onload = function(){
+    var form = document.getElementById("petBuild");
+    var petName = document.getElementById("petName")
+    var legs1 = document.getElementById("legs1");
+    var legs2 = document.getElementById("legs2");
+    var legs3 = document.getElementById("legs3");
+    var legs4 = document.getElementById("legs4");
+    var superStrength = document.getElementById("superStrength");
+    var invisability = document.getElementById("invisability");
+    var superSpeed = document.getElementById("superSpeed");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var submit = document.getElementById("submitBtn");
+
+    submit.addEventListener("click", function(){
+
+    })  
+}
+
+var pet = {
+    name: "",
+    type: "",
+    legs: "",
+    abilities: "",
+    email: "",
+    password: "",
+    description: "",
+};
+
+
+
+
+var handler = {
+
+displayEntry: function(){
+
+},
+
+clearEntry: function(){
+
+},
+
+clearEverything: function(){
+
+},
+
+
+
+};
+
+submit.addEventListener("click", function(){
+pet.name = petName.value;
+pet.type = petType.value;
+pet.email
+
+}
+
+)         
